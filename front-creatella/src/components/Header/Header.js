86c7e1137b45d466727dbffa0eb16d28420a6494
Alonso Parra <alonsogparra@gmail.com>
2019-10-28 11:48:44 -0400

    Adding Header component to the project

---
+++ front-creatella/src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const header = (props) => (
+  <nav>
+    <div className="nav-wrapper blue-grey darken-4">
+      <div className="brand-logo center">
+        Ascii Faces Store
+      </div>
+    </div>
+  </nav>
+);
+
+
+export default header;
