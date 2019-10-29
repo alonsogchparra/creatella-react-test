e2fe67ca0eab00d3ef3ea9d90178ad6078dfe60e
Alonso Parra <alonsogparra@gmail.com>
2019-10-29 10:48:01 -0400

    New actionType added LOADING_ICONS

--- front-creatella/src/store/actions/actionTypes.js
+++ front-creatella/src/store/actions/actionTypes.js
@@ -3,3 +3,4 @@ export const SORTED_BY_PRICE = 'SORTED_BY_PRICE';
 export const SORTED_BY_SIZE = 'SORTED_BY_SIZE';
 export const SORTED_BY_DATE = 'SORTED_BY_DATE';
 export const SORTED_BY_ID = 'SORTED_BY_ID';
+export const LOADING_ICONS = 'LOADING_ICONS';
