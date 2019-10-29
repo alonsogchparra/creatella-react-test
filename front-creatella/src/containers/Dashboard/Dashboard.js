dd6cd0a0f44e21c779bd27d990e6ba882f51b988
Alonso Parra <alonsogparra@gmail.com>
2019-10-29 09:17:43 -0400

    Dashboard updated with the actions and states from the store, besides there are handlers built on this container to trigger the sort action

--- front-creatella/src/containers/Dashboard/Dashboard.js
+++ front-creatella/src/containers/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header/Header';
 import IconList from '../../components/IconsList/IconList';
+import SortPanel from '../../components/SortPanel/SortPanel';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions';
 
@@ -10,13 +11,49 @@ class Dashboard extends Component {
     this.props.onGetIcons();
   }
 
+  sortedByIdHandler = () => {
+    this.props.onSortById();
+  }
+
+  sortedByPriceHandler = () => {
+    this.props.onSortByPrice();
+  }
+
+  sortedBySizeHandler = () => {
+    this.props.onSortBySize();
+  }
+
+  sortedByDateHandler = () => {
+    this.props.onSortByDate();
+  }
+
   render () {
-    const { icons } = this.props;
+    const {
+      icons,
+      sortByPrice,
+      sortBySize,
+      sortByDate,
+      sortById,
+      priceSelected,
+      sizeSelected,
+      idSelected,
+      dateSelected
+     } = this.props;
+
     return (
       <div>
         <Header />
         <div className="container">
-          <IconList icons={icons} />
+          <SortPanel
+            sortByPrice={this.sortedByPriceHandler}
+            sortBySize={this.sortedBySizeHandler}
+            sortByDate={this.sortedByDateHandler}
+            sortById={this.sortedByIdHandler}
+          />
+          { idSelected && <IconList icons={sortById} />}
+          { priceSelected && <IconList icons={sortByPrice} /> }
+          { sizeSelected && <IconList icons={sortBySize} /> }
+          { dateSelected && <IconList icons={sortByDate} /> }
         </div>
       </div>
     )
@@ -25,13 +62,25 @@ class Dashboard extends Component {
 
 const mapStateToProps = state => {
   return {
-    icons: state.icons.icons
+    icons: state.icons.icons,
+    sortByPrice: state.icons.sortByPrice,
+    sortBySize: state.icons.sortBySize,
+    sortById: state.icons.sortById,
+    sortByDate: state.icons.sortByDate,
+    priceSelected: state.icons.priceSelected,
+    sizeSelected: state.icons.sizeSelected,
+    dateSelected: state.icons.dateSelected,
+    idSelected: state.icons.idSelected
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onGetIcons: () => dispatch(actions.getIcons())
+    onGetIcons: () => dispatch(actions.getIcons()),
+    onSortByPrice: () => dispatch(actions.sortedByPrice()),
+    onSortBySize: () => dispatch(actions.sortedBySize()),
+    onSortById: () => dispatch(actions.sortedById()),
+    onSortByDate: () => dispatch(actions.sortedByDate())
   }
 }
 
