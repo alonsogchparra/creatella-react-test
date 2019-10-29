69972de3e759959504a0c5f1d417194f391af487
Alonso Parra <alonsogparra@gmail.com>
2019-10-29 09:09:57 -0400

    adding new actionTypes

--- front-creatella/src/store/actions/actionTypes.js
+++ front-creatella/src/store/actions/actionTypes.js
@@ -1,1 +1,5 @@
-export const FETCH_ICONS = 'FETCH_ICONS'
+export const FETCH_ICONS = 'FETCH_ICONS';
+export const SORTED_BY_PRICE = 'SORTED_BY_PRICE';
+export const SORTED_BY_SIZE = 'SORTED_BY_SIZE';
+export const SORTED_BY_DATE = 'SORTED_BY_DATE';
+export const SORTED_BY_ID = 'SORTED_BY_ID';
